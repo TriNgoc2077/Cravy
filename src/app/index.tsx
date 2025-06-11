0ca087f6524bce7ca16448e0780e2990ac22aa77
Cao Tri Ngoc <tringoc2077@gmail.com>
2025-06-11 09:55:45 +0700

    welcome page

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,23 +1,26 @@
-import { StyleSheet, Text, View } from "react-native";
+import bg from "@/assets/auth/background.jpg";
+import fbLogo from "@/assets/auth/Facebook.png";
+import ggLogo from "@/assets/auth/Google.png";
+import ShareButton from "@/components/button/share.button";
+import TextBetweenLine from "@/components/text.between.line";
+import { APP_COLOR } from "@/utils/constant";
+import { LinearGradient } from "expo-linear-gradient";
+import { Image, ImageBackground, StyleSheet, Text, View } from "react-native";
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    borderColor: "red",
-    borderWidth: 5,
+    paddingHorizontal: 15,
   },
   welcomeText: {
     flex: 0.6,
-    borderColor: "green",
-    borderWidth: 5,
     alignItems: "flex-start",
     justifyContent: "center",
     paddingLeft: 20,
   },
   welcomeBtn: {
     flex: 0.4,
-    borderColor: "grey",
-    borderWidth: 5,
+    gap: 30,
   },
   heading: {
     fontSize: 40,
@@ -25,49 +28,98 @@ const styles = StyleSheet.create({
   },
   name: {
     fontSize: 30,
-    color: "orange",
+    color: APP_COLOR.ORANGE,
     marginVertical: 10,
   },
   slogan: {},
-  btnContainer: {},
-  btnContent: {
-    backgroundColor: "green",
-    padding: 20,
-    borderRadius: 15,
-    alignSelf: "flex-start",
-  },
-  btnText: {
-    textTransform: "uppercase",
-  },
 });
+
 const WelcomePage = () => {
   return (
-    <View style={styles.container}>
-      <View style={styles.welcomeText}>
-        <Text style={styles.heading}>Welcome to</Text>
-        <Text style={styles.name}>Cravy</Text>
-        <Text style={styles.slogan}>Enjoy your tasty food!</Text>
-      </View>
-      <View style={styles.welcomeBtn}>
-        <Text>Login with</Text>
-        <View>
-          <View style={styles.btnContainer}>
-            <View style={styles.btnContent}>
-              <Text style={styles.btnText}>Facebook</Text>
+    <ImageBackground style={{ flex: 1 }} source={bg}>
+      <LinearGradient
+        colors={["transparent", "rgba(0,0,0,0.4)"]}
+        style={{ flex: 1 }}
+        locations={[0.5, 0.7]}>
+        <View style={styles.container}>
+          <View style={styles.welcomeText}>
+            <Text style={styles.heading}>Welcome to</Text>
+            <Text style={styles.name}>Cravy</Text>
+            <Text style={styles.slogan}>Enjoy your tasty food!</Text>
+          </View>
+          <View style={styles.welcomeBtn}>
+            <TextBetweenLine title="Login with"></TextBetweenLine>
+            <View style={{ gap: 15 }}>
+              <View
+                style={{
+                  flexDirection: "row",
+                  justifyContent: "center",
+                  gap: 30,
+                }}>
+                <ShareButton
+                  title="Facebook"
+                  onPress={() => alert("me")}
+                  textStyle={{ textTransform: "none" }}
+                  pressStyle={{ alignSelf: "flex-start" }}
+                  btnStyle={{
+                    backgroundColor: "#fff",
+                    justifyContent: "center",
+                    borderRadius: 40,
+                  }}
+                  icon={<Image source={fbLogo} />}
+                />
+                <ShareButton
+                  title="Google"
+                  onPress={() => alert("me")}
+                  textStyle={{ textTransform: "none" }}
+                  pressStyle={{ alignSelf: "flex-end" }}
+                  btnStyle={{
+                    backgroundColor: "#fff",
+                    justifyContent: "center",
+                    paddingHorizontal: 20,
+                    borderRadius: 40,
+                  }}
+                  icon={<Image source={ggLogo} />}
+                />
+              </View>
+              <View>
+                <ShareButton
+                  title="Start with your email"
+                  onPress={() => alert("me")}
+                  textStyle={{ color: "#fff", paddingVertical: 5 }}
+                  btnStyle={{
+                    justifyContent: "center",
+                    borderRadius: 30,
+                    marginHorizontal: 50,
+                    paddingVertical: 10,
+                    backgroundColor: "#2c2c2c",
+                    borderColor: "#505050",
+                    borderWidth: 1,
+                  }}
+                  pressStyle={{
+                    alignSelf: "stretch",
+                  }}></ShareButton>
+              </View>
+              <View
+                style={{
+                  flexDirection: "row",
+                  gap: 10,
+                  justifyContent: "center",
+                }}>
+                <Text style={{ color: "white" }}>Don't have an account ?</Text>
+                <Text
+                  style={{
+                    color: "white",
+                    textDecorationLine: "underline",
+                  }}>
+                  Sign up.
+                </Text>
+              </View>
             </View>
           </View>
-          <View>
-            <Text>Google</Text>
-          </View>
-          <View>
-            <Text>Start with your email</Text>
-          </View>
-          <View>
-            <Text>Don't have an account ? Sign up</Text>
-          </View>
         </View>
-      </View>
-    </View>
+      </LinearGradient>
+    </ImageBackground>
   );
 };
 
---
+++ src/components/button/share.button.tsx
@@ -0,0 +1,55 @@
+import { APP_COLOR } from "@/utils/constant";
+import { ReactNode } from "react";
+import {
+  Pressable,
+  StyleProp,
+  StyleSheet,
+  Text,
+  TextStyle,
+  View,
+  ViewStyle,
+} from "react-native";
+
+const styles = StyleSheet.create({
+  btnContainer: {
+    borderRadius: 10,
+    paddingHorizontal: 15,
+    paddingVertical: 10,
+    flexDirection: "row",
+    gap: 10,
+    alignItems: "center",
+    backgroundColor: APP_COLOR.ORANGE,
+  },
+  text: {
+    textTransform: "uppercase",
+  },
+});
+interface IProps {
+  title: string;
+  onPress: () => void;
+  textStyle?: StyleProp<TextStyle>;
+  pressStyle?: StyleProp<ViewStyle>;
+  btnStyle?: StyleProp<ViewStyle>;
+  icon?: ReactNode;
+}
+const ShareButton = (props: IProps) => {
+  const { title, onPress, textStyle, pressStyle, btnStyle, icon } = props;
+  return (
+    <Pressable
+      style={({ pressed }) => [
+        {
+          opacity: pressed ? 0.5 : 1,
+          alignSelf: "flex-start",
+        },
+        pressStyle,
+      ]}
+      onPress={onPress}>
+      <View style={[styles.btnContainer, btnStyle]}>
+        {icon}
+        <Text style={textStyle}>{title}</Text>
+      </View>
+    </Pressable>
+  );
+};
+
+export default ShareButton;
